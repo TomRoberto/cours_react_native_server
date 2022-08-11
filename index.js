1ced89b451276be4cddbbd994199770d379391e7
Tom ROBERTO <tom.roberto31@gmail.com>
2022-08-11 16:00:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+app.use(cors());
+
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const convertToBase64 = (file) => {
+  return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
+};
+
+app.put("/upload", fileUpload(), async (req, res) => {
+  try {
+    console.log(req.files);
+    const pictureToUpload = req.files.picture;
+    const result = await cloudinary.uploader.upload(
+      convertToBase64(pictureToUpload)
+    );
+    return res.json(result);
+  } catch (error) {
+    return res.json({ error: error.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server has started !");
+});
